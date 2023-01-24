5d00095b313ed1e846a25ed0e696dde432a991f0
ysk <kim206gh@naver.com>
2023-01-24 13:45:49 +0900

    feat : GlobalExceptionHandler MethodArgumentTypeMismatchException Handling 추가

--- src/main/java/com/prgrms/bdbks/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/bdbks/common/exception/GlobalExceptionHandler.java
@@ -1,107 +1,122 @@
 package com.prgrms.bdbks.common.exception;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import com.prgrms.bdbks.common.dto.ErrorResponse;
-import com.prgrms.bdbks.common.dto.ErrorResponse.FieldError;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Path;
+
 import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.prgrms.bdbks.common.dto.ErrorResponse;
+import com.prgrms.bdbks.common.dto.ErrorResponse.FieldError;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends DefaultHandlerExceptionResolver {
 
-    @ExceptionHandler(value = NullPointerException.class)
-    public ResponseEntity<ErrorResponse> handleNullPointException(
-        NullPointerException e, HttpServletRequest request) {
+	@ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+		MethodArgumentTypeMismatchException e, HttpServletRequest request
+	) {
+		ErrorResponse errorResponse = ErrorResponse.badRequest(e.getParameter().getParameterName(),
+			request.getRequestURI(),
+			List.of(new FieldError(e.getName(), Objects.requireNonNull(e.getValue()).toString(), e.getMessage())));
+		return ResponseEntity.badRequest().body(errorResponse);
+	}
 
-        return ResponseEntity.badRequest()
-            .body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI(), null));
-    }
+	@ExceptionHandler(value = NullPointerException.class)
+	public ResponseEntity<ErrorResponse> handleNullPointException(
+		NullPointerException e, HttpServletRequest request) {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    protected ResponseEntity<ErrorResponse> handleConstraintViolationException(
-        HttpServletRequest request,
-        ConstraintViolationException e) {
+		return ResponseEntity.badRequest()
+			.body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI(), null));
+	}
 
-        return ResponseEntity.badRequest()
-            .body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()
-                , makeFieldErrorsFromConstraintViolations(e.getConstraintViolations())));
-    }
+	@ExceptionHandler(ConstraintViolationException.class)
+	protected ResponseEntity<ErrorResponse> handleConstraintViolationException(
+		HttpServletRequest request,
+		ConstraintViolationException e) {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(HttpServletRequest request,
-        SQLIntegrityConstraintViolationException e) {
+		return ResponseEntity.badRequest()
+			.body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()
+				, makeFieldErrorsFromConstraintViolations(e.getConstraintViolations())));
+	}
 
-        return ResponseEntity.badRequest()
-            .body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()));
-    }
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorResponse> handleRuntimeException(HttpServletRequest request,
+		SQLIntegrityConstraintViolationException e) {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(
-        HttpServletRequest request,
-        IllegalArgumentException e) {
+		return ResponseEntity.badRequest()
+			.body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()));
+	}
 
-        return ResponseEntity.badRequest()
-            .body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()));
-    }
+	@ExceptionHandler(IllegalArgumentException.class)
+	protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+		HttpServletRequest request,
+		IllegalArgumentException e) {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
-        HttpServletRequest request,
-        MethodArgumentNotValidException e) {
+		return ResponseEntity.badRequest()
+			.body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()));
+	}
 
-        return ResponseEntity.badRequest().body(ErrorResponse.badRequest(
-            e.getMessage(), request.getRequestURI(),
-            makeFieldErrorsFromBindingResult(e.getBindingResult())
-        ));
-    }
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+		HttpServletRequest request,
+		MethodArgumentNotValidException e) {
 
-    @ExceptionHandler(InvalidFormatException.class)
-    protected ResponseEntity<ErrorResponse> handleInvalidFormatException(
-        HttpServletRequest request,
-        InvalidFormatException e) {
+		return ResponseEntity.badRequest().body(ErrorResponse.badRequest(
+			e.getMessage(), request.getRequestURI(),
+			makeFieldErrorsFromBindingResult(e.getBindingResult())
+		));
+	}
 
-        return ResponseEntity.badRequest()
-            .body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()));
-    }
+	@ExceptionHandler(InvalidFormatException.class)
+	protected ResponseEntity<ErrorResponse> handleInvalidFormatException(
+		HttpServletRequest request,
+		InvalidFormatException e) {
 
-    private List<FieldError> makeFieldErrorsFromBindingResult(BindingResult bindingResult) {
-        List<FieldError> fieldErrors = new ArrayList<>();
+		return ResponseEntity.badRequest()
+			.body(ErrorResponse.badRequest(e.getMessage(), request.getRequestURI()));
+	}
 
-        for (org.springframework.validation.FieldError fieldError : bindingResult.getFieldErrors()) {
-            FieldError error = FieldError.of(fieldError.getField(), Objects.requireNonNull(
-                fieldError.getRejectedValue()), fieldError.getDefaultMessage());
-            fieldErrors.add(error);
-        }
+	private List<FieldError> makeFieldErrorsFromBindingResult(BindingResult bindingResult) {
+		List<FieldError> fieldErrors = new ArrayList<>();
 
-        return fieldErrors;
-    }
+		for (org.springframework.validation.FieldError fieldError : bindingResult.getFieldErrors()) {
+			FieldError error = FieldError.of(fieldError.getField(), Objects.requireNonNull(
+				fieldError.getRejectedValue()), fieldError.getDefaultMessage());
+			fieldErrors.add(error);
+		}
 
-    private List<FieldError> makeFieldErrorsFromConstraintViolations(
-        Set<ConstraintViolation<?>> constraintViolations) {
+		return fieldErrors;
+	}
 
-        return constraintViolations.stream()
-            .map(violation -> FieldError.of(getFieldFromPath(violation.getPropertyPath()),
-                violation.getInvalidValue(), violation.getMessage()))
-            .collect(Collectors.toList());
-    }
+	private List<FieldError> makeFieldErrorsFromConstraintViolations(
+		Set<ConstraintViolation<?>> constraintViolations) {
 
-    private String getFieldFromPath(Path fieldPath) {
-        PathImpl pathImpl = (PathImpl) fieldPath;
-        return pathImpl.getLeafNode().toString();
-    }
+		return constraintViolations.stream()
+			.map(violation -> FieldError.of(getFieldFromPath(violation.getPropertyPath()),
+				violation.getInvalidValue(), violation.getMessage()))
+			.collect(Collectors.toList());
+	}
+
+	private String getFieldFromPath(Path fieldPath) {
+		PathImpl pathImpl = (PathImpl)fieldPath;
+		return pathImpl.getLeafNode().toString();
+	}
 
 }
