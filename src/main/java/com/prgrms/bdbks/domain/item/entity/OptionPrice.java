13db8ac3ffe6e42c55159e9c5bf4b5160f33db6d
youngji <youngji.jang804@gmail.com>
2023-01-25 20:50:17 +0900

    feat : optionPrice Bean 등록

--- src/main/java/com/prgrms/bdbks/domain/item/entity/OptionPrice.java
+++ src/main/java/com/prgrms/bdbks/domain/item/entity/OptionPrice.java
@@ -2,13 +2,16 @@ package com.prgrms.bdbks.domain.item.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+// TODO: 2023/01/25 마지막에 시간남으면 jdbc화
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class OptionPrice {
 
-    private final int espressoPrice;
+	private int espressoPrice = 600;
 
-    private final int syrupPrice;
+	private int syrupPrice = 600;
 
 }
