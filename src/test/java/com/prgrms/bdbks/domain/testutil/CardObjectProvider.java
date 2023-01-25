f8d6cd9d2bf02e7e04b65bb9cbfeececf39d9c16
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-25 17:44:55 +0900

    test : Card 전체조회, 단건조회, 충전 통합테스트, 카드 충전 단위테스트 추가

--- src/test/java/com/prgrms/bdbks/domain/testutil/CardObjectProvider.java
+++ src/test/java/com/prgrms/bdbks/domain/testutil/CardObjectProvider.java
@@ -1,5 +1,6 @@
 package com.prgrms.bdbks.domain.testutil;
 
+import com.prgrms.bdbks.domain.card.dto.CardChargeRequest;
 import com.prgrms.bdbks.domain.card.entity.Card;
 import com.prgrms.bdbks.domain.user.entity.User;
 
@@ -12,6 +13,19 @@ public class CardObjectProvider {
 	public static Card createCard(User user) {
 		return Card.builder()
 			.user(user)
+			.name("카드카드카드")
 			.build();
+	}
+
+	public static Card createCard(User user, String id) {
+		return Card.builder()
+			.id(id)
+			.user(user)
+			.name("카드카드카드")
+			.build();
+	}
+
+	public static CardChargeRequest createCardRequest(String id, int amount) {
+		return new CardChargeRequest(id, amount);
 	}
 }
