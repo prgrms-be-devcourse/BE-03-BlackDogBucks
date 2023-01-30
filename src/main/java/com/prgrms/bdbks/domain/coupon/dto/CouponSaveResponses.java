494c2123eeb7289e1a2945976b6a3b2fb806d785
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-30 18:15:14 +0900

    feat : Star, Coupon dto 추가 - CouponSaveResponses - StarExchangeResponse

---
+++ src/main/java/com/prgrms/bdbks/domain/coupon/dto/CouponSaveResponses.java
@@ -0,0 +1,13 @@
+package com.prgrms.bdbks.domain.coupon.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CouponSaveResponses {
+
+	private List<CouponSaveResponse> couponSaveResponses;
+}
