af70ac13d85b54542c8036dd9c029053c28c6e18
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-06 16:32:13 +0900

    test : Card id 커스텀 생성 기능 테스트

--- src/test/java/com/prgrms/bdbks/domain/card/repository/CardIdGeneratorTest.java
+++ src/test/java/com/prgrms/bdbks/domain/card/repository/CardIdGeneratorTest.java
@@ -1,12 +1,9 @@
 package com.prgrms.bdbks.domain.card.repository;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import java.util.Properties;
-
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.type.Type;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,24 +16,18 @@ class CardIdGeneratorTest {
 	@Test
 	public void testNamedGenerator() {
 
+		int idLength = 23;
+
 		SharedSessionContractImplementor mockSession = mock(SharedSessionContractImplementor.class);
 
 		Object mockObject = mock(Object.class);
 
-		Type type = mock(Type.class);
-
-		Properties params = new Properties();
-
-		params.put("userId", "userId는여기에 넘겨야 한다.");
-
-		ServiceRegistry serviceRegistry = mock(ServiceRegistry.class);
-
-		cardIdGenerator.configure(type, params, serviceRegistry);
-
-		String generatedId = cardIdGenerator.generate(mockSession, mockObject).toString();
+		String generatedId = this.cardIdGenerator.generate(mockSession, mockObject).toString();
 
 		log.info("id = {}", generatedId);
 
+		assertEquals(idLength, generatedId.length());
+		assertTrue(generatedId.contains("HK"));
 	}
 
 }
