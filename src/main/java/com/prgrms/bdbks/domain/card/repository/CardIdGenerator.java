9cbce963f36a07dc8b190b5a2efba07b0e7afe5b
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-07 00:08:30 +0900

    refactor : card id Generator 로직 수정

--- src/main/java/com/prgrms/bdbks/domain/card/repository/CardIdGenerator.java
+++ src/main/java/com/prgrms/bdbks/domain/card/repository/CardIdGenerator.java
@@ -1,23 +1,35 @@
 package com.prgrms.bdbks.domain.card.repository;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import com.prgrms.bdbks.domain.card.entity.Card;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class CardIdGenerator implements IdentifierGenerator {
+
+	private final RandomNumberGenerator randomNumberGenerator;
 
 	@Override
 	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
 
-		String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
-		String randomId = RandomStringUtils.randomAlphabetic(4);
+		if (object instanceof Card) {
+			Card card = (Card)object;
+			Long userId = card.getUser().getId();
 
-		return time + "HK" + randomId;
+			int randomNumber = randomNumberGenerator.getRandom();
+
+			long userIdEncryption = userId % 9000 + 1000;
+
+			return userIdEncryption + "-" + randomNumber;
+		}
+
+		return new IllegalArgumentException("올바른 Id Generator가 아닙니다.");
 	}
 
 }
